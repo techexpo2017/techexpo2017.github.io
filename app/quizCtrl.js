eef57142d0603de2a7fd2cc5626f8b4200087d12
Lakshmanan,Murali(mulakshmanan) <mulakshmanan@paypal.com>
2017-11-21 17:00:44 +0530

    Last changes

--- app/quizCtrl.js
+++ app/quizCtrl.js
@@ -14,7 +14,7 @@
     $scope.defaultConfig = {
         'allowBack': true,
         'allowReview': true,
-        'autoMove': true,  // if true, it will move to next question automatically when answered.
+        'autoMove': false,  // if true, it will move to next question automatically when answered.
         'duration': 0,  // indicates the time in which quiz needs to be completed. post that, quiz will be automatically submitted. 0 means unlimited.
         'pageSize': 1,
         'requiredAll': false,  // indicates if you must answer all the questions before submitting.
